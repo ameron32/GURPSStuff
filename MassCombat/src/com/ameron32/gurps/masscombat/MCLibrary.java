9dc32bfc2f86a4dbc51d2c7a60fe4ed8c821627f
Kris L <ameron32extras@gmail.com>
2014-01-23 17:00:17 -0600

    a few more standard elements and wt to dash

--- MassCombat/src/com/ameron32/gurps/masscombat/MCLibrary.java
+++ MassCombat/src/com/ameron32/gurps/masscombat/MCLibrary.java
@@ -63,28 +63,38 @@ public static final boolean DEBUG = false;
       
 
     // store("Balloon; (1); Air; 2; 0; 50K; 5K; 5");
+  	// needs (support only)
     store("Bowmen; 2; F; 1; Foot; 40K; 8K; 2");
     store("Cavalry Pistols 4; 3; Cv,F; 2; Mtd; 100K; 20K; 4");
     store("Cavalry Pistols 5; 6; Cv,F; 2; Mtd; 100K; 20K; 5");
-    // store("Draft Team; 0; T; 2; Mtd; 10K; 1K; 1");
-    // store(heavy artillery 2));
-    // store(heavy artillery 5));
+    store("Draft Team; 0; T; 2; Mtd; 10K; 1K; 1");
+    // store("Heavy Artillery"; 2.5*; Art; 2; Foot; 100K; 10K; 2"));
+    // store("Heavy Artillery 3; (5); Art; 2; Foot; 100K; 10K; 3");
+    // store("Heavy Artillery 4; (10); Art; 2; Foot; 100K; 10K; 4");
+    // store("Heavy Artillery 5; (20); Art; 2; Foot; 100K; 10K; 5");
+    // needs (support only)
     store("Heavy Cavalry; 5; Cv; 2; Mtd; 200K; 40K; 2");
     store("Heavy Chariots; 4; Cv; 4; Mtd; 160K; 32K; 1");
-     store("Heavy Infantry; 4; –; 1; Foot; 40K; 8K; 2");
+    store("Heavy Infantry; 4; –; 1; Foot; 40K; 8K; 2");
     store("Horse Archers; 2; Cv,F,Rec; 2; Mtd; 120K; 24K; 2");
     store("Horse Artillery; 10; Art; 2; Mtd; 150K; 30K; 5");
     // store("Light Artillery; (1)*; Art; 1; Foot; 40K; 8K; 2");
+    // needs all TLs & (support only)
     store("Light Cavalry; 2; Cv,Rec; 2; Mtd; 100K; 20K; 2");
     store("Light Chariots; 2; Cv,F; 4; Mtd; 100K; 20K; 1");
     store("Light Infantry; 2; Rec; 1; Foot; 40K; 8K; 1");
     store("Line Infantry; 3; F; 1; Foot; 30K; 6K; 5");
     store("Medium Cavalry; 3; Cv,F; 2; Mtd; 150K; 30K; 2");
-     store("Medium Infantry; 3; –; 1; Foot; 30K; 6K; 1");
+    store("Medium Infantry; 3; –; 1; Foot; 30K; 6K; 1");
     // store("Miners; 0.5*; Eng; 1; Foot; 30K; 6K; 2");
+    // needs float?
+    store("Miners 3; 1; Eng; 1; Foot; 30K; 6K; 3");
+    store("Miners 4; 2; Eng; 1; Foot; 30K; 6K; 4");
+    store("Miners 5; 4; Eng; 1; Foot; 30K; 6K; 5");
     store("Mounts; 0; T1; 1; Mtd; 60K; 12K; 1");
     store("Musketeers; 2; F; 1; Foot; 30K; 6K; 4");
     // store("Pikemen; 4; (Cv); 1; Foot; 60K; 12K; 2");
+    // needs (Cv) resolved
     store("Skirmishers; 2; F,Rec; 1; Foot; 30K; 6K; 5");
     store("Stone-Age Warriors; 1; Rec; 1; Foot; 25K; 5K; 0");
     store("War Beast; 20; Arm; 4; Foot; 400K; 80K; 2");
@@ -96,33 +106,33 @@ public static final boolean DEBUG = false;
     store("Flying Beasts; 1; Air,T1; 2; SA; 120K; 40K; 0");
     store("Flying Cavalry; 2; Air,F; 2; SA; 600K; 60K; 1");
     store("Flying Infantry; 2; Air,Rec; 1; Foot,SA; 60K; 20K; 0");
-    store("Flying Leviathan; 150; Air,T10; 10000; SA; 1000K; 40K; 0");
+    store("Flying Leviathan; 150; Air,T10; –; SA; 1000K; 40K; 0");
     store("Flying Mages; 5; Air,Art,C3I,F,Rec; 1; Foot,SA; 300K; 60K; 1");
     store("Giant Flying Monster; 15; Air; 8; SA; 800K; 40K; 0");
     store("Giant Monster; 40; Arm; 8; Foot; 800K; 40K; 0");
     store("Giants; 20; Arm,Art,Eng; 8; Foot; 800K; 40K; 0");
-    store("Leviathan; 250; Nav,T10; 10000; Sea; 10000K; 400K; 0");
+    store("Leviathan; 250; Nav,T10; –; Sea; 10000K; 400K; 0");
     store("Ogres; 8; –; 4; Foot; 80K; 8K; 0");
-    store("Sea Monster; 20; Nav,T1; 10000; Sea; 1000K; 20K; 0");
-    store("Titan; 400; Arm,Art,Eng; 10000; Foot; 800K; 40K; 0");
+    store("Sea Monster; 20; Nav,T1; –; Sea; 1000K; 20K; 0");
+    store("Titan; 400; Arm,Art,Eng; –; Foot; 800K; 40K; 0");
 
     store("Boat; 0; T1; 1; Coast; 5K; 0.5K; 1");
-    store("Brig; 6; Nav,Art,T6; 10000; Sea; 150K; 15K; 4");
-    store("Cog; 4; T5; 10000; Sea; 75K; 7.5K; 3");
-    store("Frigate; 150; Nav,Art,T4; 10000; Sea; 1000K; 50K; 5");
-    store("Galleon; 30; Nav,Art,T6; 10000; Sea; 750K; 75K; 4");
+    store("Brig; 6; Nav,Art,T6; –; Sea; 150K; 15K; 4");
+    store("Cog; 4; T5; –; Sea; 75K; 7.5K; 3");
+    store("Frigate; 150; Nav,Art,T4; –; Sea; 1000K; 50K; 5");
+    store("Galleon; 30; Nav,Art,T6; –; Sea; 750K; 75K; 4");
     store("Large Boat; 0; T2; 1; Coast; 10K; 1K; 1");
-    store("Light Galley; 3; T1; 10000; Coast; 70K; 14K; 1");
-    store("Longship; 3; T7; 10000; Coast; 50K; 10K; 2");
-    store("Merchant Galley; 4; Nav,T5; 10000; Coast; 600K; 60K; 2");
-    store("Ship-of-the-Line; 300; Nav,Art,T10; 10000; Sea; 4000K; 400K; 5");
-    store("War Galley; 10; Nav,T3; 10000; Coast; 500K; 100K; 2");
+    store("Light Galley; 3; T1; –; Coast; 70K; 14K; 1");
+    store("Longship; 3; T7; –; Coast; 50K; 10K; 2");
+    store("Merchant Galley; 4; Nav,T5; –; Coast; 600K; 60K; 2");
+    store("Ship-of-the-Line; 300; Nav,Art,T10; –; Sea; 4000K; 400K; 5");
+    store("War Galley; 10; Nav,T3; –; Coast; 500K; 100K; 2");
 
   }
   
-  // Example "Bowmen; 2; F; 1; Foot; 40K; 8K; 2"
   /**
    * Hand it a human-readable string and it builds and hands back the corresponding Element.
+   * Example "Bowmen; 2; F; 1; Foot; 40K; 8K; 2"
    * 
    * @param elementCode
    * @return
@@ -197,11 +207,19 @@ public static final boolean DEBUG = false;
       mobilities[l] = Mobility.Type.valueOf(sMobilities[l]);
     }
     
+    // uncarriable WT
+    int transportWeight = 0;
+    if (pieces[wt].equalsIgnoreCase("–")) {
+      transportWeight = 10000;
+    } else {
+      transportWeight = Integer.decode(pieces[wt]);
+    }
+    
     Element e = Element.newLibraryStandard(
         pieces[element], 
         Integer.decode(pieces[ts]), 
         getSpecialClasses(scSpecialClasses), 
-        Integer.decode(pieces[wt]), 
+        transportWeight, 
         getMobility(mobilities), 
         Math.round((Float.parseFloat(pieces[raise]))*1000), 
         Math.round((Float.parseFloat(pieces[maintain]))*1000), 
