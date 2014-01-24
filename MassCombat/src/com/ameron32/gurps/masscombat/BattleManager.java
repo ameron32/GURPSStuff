a88198f305d7b40a64b51015b1b33e110d930276
ameron32 <ameron32extras@gmail.com>
2014-01-23 20:45:29 -0600

    Characters and BattleManager framework laid

---
+++ MassCombat/src/com/ameron32/gurps/masscombat/BattleManager.java
@@ -0,0 +1,63 @@
+package com.ameron32.gurps.masscombat;
+
+public class BattleManager {
+
+	boolean inBattle = false;
+	boolean canceled = false;
+	
+	void prepare() {
+		initiativeCommand();
+	}
+	
+	void begin() {
+		inBattle = true;
+		beforeBattle();
+		while(inBattle) {
+			battleRound();
+			
+			// postBattleRound/preBattleRound
+			if (totalCasualties) {	
+				// end the Battle
+				inBattle = false;
+			}
+			else {
+				// prepare for next round
+				recalcBasicStrategyModifier();
+				reinitIntelligence();
+			}
+		}
+		if (!canceled) afterBattle();		
+	}
+	
+	private void beforeBattle() {
+		calcBasicStrategyModifier();
+		calcDefensiveBonus(); 
+		initIntelligence();
+		
+	}
+	
+	private void battleRound() {
+		chooseRisk();
+		chooseBattleStrategy();
+		calcStrategyEffectiveness();
+		calcMisfortuneOfWar();
+		
+	}
+	
+	private void afterBattle() {
+		regroupCasualties();
+		calcLogisticForceCasualty();
+		dispenseLoot();
+		determineCaptives();
+	}
+	
+	void conclude() {
+		inBattle = false;
+	}
+	
+	void cancel() {
+		inBattle = false;
+		canceled = true;
+	}
+	
+}
---
+++ MassCombat/src/com/ameron32/gurps/masscombat/Characters.java
@@ -0,0 +1,61 @@
+package com.ameron32.gurps.masscombat;
+
+import java.util.*;
+
+public class Characters
+ {
+	
+	public static class Character {
+		public String name;
+		
+		protected Skill strategy = new Skill("Strategy");
+		protected Skill leadership = new Skill("Leadership");
+		protected Skill intelligenceAnalysis = new Skill("Intelligence Analysis");
+		protected Skill administration = new Skill("Administration");
+		
+		private Character() {}
+		
+		class Skill {
+			Skill(String name){this.name = name;}
+			public String name;
+			public int level;
+		}
+	}
+	
+	public static class Leader extends Character {
+		Type type;
+		Force ofForce;
+		
+		public static Leader generateLeader() {
+			Leader leader = new Leader();
+			Random r = new java.util.Random();
+			final int minimumRandomSkill = 7;
+			final int skillSpread = 13;
+			
+			// generate skills
+			leader.strategy.level = r.nextInt(skillSpread) + minimumRandomSkill;
+			leader.leadership.level = r.nextInt(skillSpread) + minimumRandomSkill;
+			leader.intelligenceAnalysis.level = r.nextInt(skillSpread) + minimumRandomSkill;
+			leader.administration.level = r.nextInt(skillSpread) + minimumRandomSkill;
+			
+			// generate AWESOME name!
+			leader.name = "General " + r.nextInt(100);
+			
+			return leader;
+		}
+		
+		private Leader() {}
+		public void appointLeader(Leader.Type type, Force ofForce) {
+			this.type = type;
+			this.ofForce = ofForce;
+		} 
+		
+		public enum Type {
+			Commander, IntelligenceChief, Quartermaster
+		}
+	}
+	
+	public static class Hero extends Character {
+		
+	}
+}
