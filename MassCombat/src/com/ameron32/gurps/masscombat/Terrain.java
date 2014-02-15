2431709b74444afe508b1d717433686701606d93
ameron32 <ameron32extras@gmail.com>
2014-02-14 22:41:39 -0600

    TerrainRating

--- MassCombat/src/com/ameron32/gurps/masscombat/Terrain.java
+++ MassCombat/src/com/ameron32/gurps/masscombat/Terrain.java
@@ -28,7 +28,50 @@ public class Terrain {
     return value;
   }
   
+  static int getTerrainRating(Terrain.Type type) {
+    int value = -1;
+    switch (type) {
+    case OpenWater:
+    case Plains:
+      value = 8;
+      break;
+    case Desert:
+      value = 7;
+      break;
+    case Arctic:
+    case BuiltUpAreas:
+    case Hills:
+    case Island:
+    case Beach:
+    case Coast:
+    case Rural:
+      value = 6;
+      break;
+    case Woodlands:
+    case RuralHedgerows:
+      value = 5;
+      break;
+    case Mountain:
+    case Swampland:
+    case Underwater:
+    case TracklessWoodlands:
+      value = 4;
+      break;
+    case BuiltUpAreasFromWithinArea:
+    case Jungle:
+      value = 3;
+      break;
+    // no references directly to Road, goodRoad, while OffRoad should be one of the above
+    default:
+      value = 0;
+      break;
+    }
+    return value;
+  }
+  
   public enum Type {
-    OpenWater, OffRoad, Road, GoodRoad, Coast
+    OpenWater, OffRoad, Road, GoodRoad, Coast,
+      Arctic, BuiltUpAreas, BuiltUpAreasFromWithinArea, Desert, Hills, Island, Beach, Jungle, 
+      Mountain, Plains, Rural, RuralHedgerows, Swampland, Underwater, Woodlands, TracklessWoodlands
   }
 }
